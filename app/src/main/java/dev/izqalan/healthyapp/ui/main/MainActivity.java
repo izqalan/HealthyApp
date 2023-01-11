153019c5a44c9ec8140155752fda2aa6e3c89142
Izqalan Nor'izad <izqalan@Izqalans-MacBook-Pro.local>
2023-01-11 12:11:10 +0800

    added bottom navigation view

--- app/src/main/java/dev/izqalan/healthyapp/ui/main/MainActivity.java
+++ app/src/main/java/dev/izqalan/healthyapp/ui/main/MainActivity.java
@@ -3,6 +3,9 @@ package dev.izqalan.healthyapp.ui.main;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -16,6 +19,7 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.functions.FirebaseFunctions;
 import com.google.firebase.functions.HttpsCallableResult;
@@ -24,47 +28,52 @@ import java.util.Map;
 
 import dev.izqalan.healthyapp.R;
 import dev.izqalan.healthyapp.api.CloudFunctions;
+import dev.izqalan.healthyapp.databinding.ActivityMainBinding;
 import dev.izqalan.healthyapp.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        TextView textView = findViewById(R.id.tv_message);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        BottomNavigationView bottomNav = findViewById(R.id.nav_view);
+
         Toolbar toolbar = findViewById(R.id.toolbar_main);
         setSupportActionBar(toolbar);
 
-        CloudFunctions cloudFunctions = new CloudFunctions();
-
-        FirebaseFunctions mFunctions = FirebaseFunctions.getInstance();
-
-        mFunctions.getHttpsCallable("helloWorld")
-                .call()
-                .continueWith(new Continuation<HttpsCallableResult, String>() {
-                    @Override
-                    public String then(@NonNull Task<HttpsCallableResult> task) throws Exception {
-                        // This continuation runs on either success or failure, but if the task
-                        // has failed then getResult() will throw an Exception which will be
-                        // propagated down.
-                        String result = (String) task.getResult().getData();
-                        return result;
-                    }
-                }).addOnCompleteListener(new OnCompleteListener<String>() {
-                    @Override
-                    public void onComplete(@NonNull Task<String> task) {
-                        if (task.isSuccessful()) {
-                            String result = task.getResult();
-                            textView.setText(result);
-                        } else {
-                            task.getException().printStackTrace();
-                        }
-                    }
-                });
-
+        bottomNav.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.menu_home:
+                    Log.d("nav", "pressed home");
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.menu_bmi:
+                    Log.d("nav", "pressed bmi");
+                    replaceFragment(new BmiFragment());
+                    break;
+                case R.id.menu_bp:
+                    Log.d("nav", "pressed bp");
+                    replaceFragment(new BPFragment());
+                    break;
+            }
+            return true;
+        });
     }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -84,10 +93,10 @@ public class MainActivity extends AppCompatActivity {
                 intent = new Intent(this, LoginActivity.class);
                 startActivity(intent);
                 break;
-
             case R.id.menu_profile:
                 Toast.makeText(this, "this is profile", Toast.LENGTH_SHORT).show();
                 break;
+
             default:
                 break;
         }
