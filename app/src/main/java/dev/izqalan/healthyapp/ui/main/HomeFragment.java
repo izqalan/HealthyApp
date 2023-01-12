7cb60a44870d042ae027d8d4442288795467d213
Izqalan Nor'izad <izqalan@Izqalans-MacBook-Pro.local>
2023-01-12 17:58:30 +0800

    added cloud functions

--- app/src/main/java/dev/izqalan/healthyapp/ui/main/HomeFragment.java
+++ app/src/main/java/dev/izqalan/healthyapp/ui/main/HomeFragment.java
@@ -2,11 +2,24 @@ package dev.izqalan.healthyapp.ui.main;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.functions.FirebaseFunctions;
+import com.google.firebase.functions.HttpsCallableResult;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import dev.izqalan.healthyapp.R;
 
@@ -21,6 +34,8 @@ public class HomeFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private TextView textView;
+    FirebaseAuth mAuth;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -55,12 +70,41 @@ public class HomeFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        textView = rootView.findViewById(R.id.textView);
+        FirebaseFunctions mFunctions = FirebaseFunctions.getInstance();
+
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        HashMap<String, String> data = new HashMap<>();
+        data.put("uid", currentUser.getUid());
+        Object response = mFunctions.getHttpsCallable("getUserBiodata")
+                .call(data)
+                .continueWith(new Continuation<HttpsCallableResult, Object>() {
+                    @Override
+                    public Object then(@NonNull Task<HttpsCallableResult> task) throws Exception {
+                        if (task.isSuccessful()){
+                            Log.d("user biodata", task.getResult().getData().toString());
+                            textView.setText("user exist");
+                        } else {
+                            Log.d("cloud functions", "call failed");
+                            task.getException().printStackTrace();
+                        }
+                        return task.getResult().getData();
+                    }
+                });
+
+        return rootView;
     }
+
+
 }
---
+++ functions/functions/index.js
@@ -0,0 +1,157 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+admin.initializeApp();
+
+// // Create and deploy your first functions
+// // https://firebase.google.com/docs/functions/get-started
+//
+exports.helloWorld = functions.https.onCall(() => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send({
+    message: "Hello from Firebase!",
+  });
+});
+
+// update user biodata, if doesn't exit create one
+exports.updateUserBiodata = functions.https.onCall((data) => {
+  const { uid, name, age, weight, height } = data;
+  const db = admin.firestore();
+  const userRef = db.collection("users").doc(uid);
+  return userRef
+    .get() // get user biodata
+    .then((doc) => {
+      if (doc.exists) {
+        // if user biodata exist, update it
+        return userRef.update({ name, age, weight, height });
+      } else {
+        // if user biodata doesn't exist, create one
+        return userRef.set({ name, age, weight, height });
+      }
+    }
+    )
+    .then(() => {
+      return { 
+        message: "User biodata updated successfully",
+        name,
+        age,
+        weight,
+        height
+       };
+    }
+    )
+    .catch((error) => {
+      return { error };
+    }
+    );
+});
+
+// get user biodata by uid
+exports.getUserBiodata = functions.https.onCall((data) => {
+  const { uid } = data;
+  const db = admin.firestore();
+  const userRef = db.collection("biodata").doc(uid);
+  return userRef
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        return doc.data();
+      } else {
+        return { message: "User biodata not found" };
+      }
+    }
+    )
+    .catch((error) => {
+      return { error };
+    }
+    );
+});
+
+// add user bmi to collectiontion history > bmi > user id collection
+exports.addUserBmi = functions.https.onCall((data) => {
+  const { uid, height, weight } = data;
+  // calculate bmi
+  const calculated = (weight / (height * height)) * 10000;
+  const timestamp = admin.firestore.FieldValue.serverTimestamp();
+  const db = admin.firestore();
+  const userRef = db.collection("history").doc("bmi").collection(uid);
+  return userRef
+    .add({ height, weight, calculated, created_at: timestamp })
+    .then(() => {
+      return { 
+        message: "User bmi added successfully",
+        bmi: calculated 
+      };
+    })
+    .catch((error) => {
+      return { error };
+    }
+    );
+});
+
+// get user bmi history by uid
+
+exports.getUserBmiHistory = functions.https.onCall((data) => {
+  const { uid } = data;
+  const db = admin.firestore();
+  const userRef = db.collection("history").doc("bmi").collection(uid);
+  return userRef
+    .get()
+    .then((snapshot) => {
+      if (snapshot.empty) {
+        return { message: "User bmi history not found" };
+      } else {
+        const data = [];
+        snapshot.forEach((doc) => {
+          data.push(doc.data());
+        });
+        return data;
+      }
+    })
+    .catch((error) => {
+      return { error };
+    });
+  });
+
+// add user blood pressure reading to collectiontion history > blood pressure > user id collection
+exports.addUserBloodPressure = functions.https.onCall((data) => {
+  const { uid, sys, dia, bp } = data;
+  const timestamp = admin.firestore.FieldValue.serverTimestamp();
+  const db = admin.firestore();
+  const userRef = db.collection("history").doc("blood_pressure").collection(uid);
+  return userRef
+    .add({ sys, dia, bp, created_at: timestamp })
+    .then(() => {
+      return { 
+        message: "User blood pressure added successfully",
+     };
+    })
+    .catch((error) => {
+      return { error };
+    }
+    );
+});
+
+// get user blood pressure
+
+exports.getUserBloodPressureHistory = functions.https.onCall((data) => {
+  const { uid } = data;
+  const db = admin.firestore();
+  const userRef = db.collection("history").doc("blood_pressure").collection(uid);
+  return userRef
+    .get()
+    .then((snapshot) => {
+      if (snapshot.empty) {
+        return { message: "User blood pressure history not found" };
+      } else {
+        const data = [];
+        snapshot.forEach((doc) => {
+          data.push(doc.data());
+        });
+        return data;
+      }
+    })
+    .catch((error) => {
+      return { error };
+    });
+  });
