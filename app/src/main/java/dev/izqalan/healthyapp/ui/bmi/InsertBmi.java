f11b274b8bab1d73fe184b88e7ce02700ea36a0a
Nurudddin <dinsuprano@gmail.com>
2023-01-14 12:18:51 +0800

    create bmi and bp insert form

--- app/src/main/java/dev/izqalan/healthyapp/ui/bmi/InsertBmi.java
+++ app/src/main/java/dev/izqalan/healthyapp/ui/bmi/InsertBmi.java
@@ -1,14 +1,10 @@
-package dev.izqalan.healthyapp.ui.profile;
+package dev.izqalan.healthyapp.ui.bmi;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ActionBar;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,24 +20,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 import dev.izqalan.healthyapp.R;
-import dev.izqalan.healthyapp.ui.main.MainActivity;
 
-public class ProfileUpdate extends AppCompatActivity {
 
-    EditText et_name, et_age, et_weight, et_height;
-    Button submitButton;
-
+public class InsertBmi extends AppCompatActivity {
+    EditText et_weight,et_height;
+    Button save;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile_update);
+        setContentView(R.layout.activity_insert_bmi);
 
+        et_weight = findViewById(R.id.et_weight_bmi);
+        et_height = findViewById(R.id.et_height_bmi);
+        save = findViewById(R.id.save_bmi_button);
 
-        et_name = findViewById(R.id.et_name);
-        et_age = findViewById(R.id.et_age);
-        et_height = findViewById(R.id.et_height);
-        et_weight = findViewById(R.id.et_weight);
-        submitButton = findViewById(R.id.submit_button);
 
         FirebaseFunctions mFunctions = FirebaseFunctions.getInstance();
         FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
@@ -49,44 +41,21 @@ public class ProfileUpdate extends AppCompatActivity {
         HashMap<String, String> data = new HashMap<>();
         data.put("uid", currentUser.getUid());
 
-        mFunctions.getHttpsCallable("getUserBiodata")
-                .call(data)
-                .continueWith(new Continuation<HttpsCallableResult, Object>() {
-                    @Override
-                    public Object then(@NonNull Task<HttpsCallableResult> task) throws Exception {
-                        if (task.isSuccessful()){
-                            Map<String, Object> response = (Map<String, Object>) task.getResult().getData();
-
-                            if (response.size() > 1){
-                                et_name.setText(response.get("name").toString());
-                                et_age.setText(response.get("age").toString());
-                                et_height.setText(response.get("height").toString());
-                                et_weight.setText(response.get("weight").toString());
-                            }
-                        } else {
-                            Log.d("err biodata", task.getResult().getData().toString());
-                        }
-                        return null;
-                    }
-                });
-
-        submitButton.setOnClickListener(new View.OnClickListener() {
+        save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 HashMap<String, String> data = new HashMap<>();
                 data.put("uid", currentUser.getUid());
-                data.put("name", et_name.getText().toString());
-                data.put("age", et_age.getText().toString());
-                data.put("height", et_height.getText().toString());
                 data.put("weight", et_weight.getText().toString());
+                data.put("height", et_height.getText().toString());
 
-                mFunctions.getHttpsCallable("updateUserBiodata")
+                mFunctions.getHttpsCallable("addUserBmi ")
                         .call(data)
                         .continueWith(new Continuation<HttpsCallableResult, Object>() {
                             @Override
                             public Object then(@NonNull Task<HttpsCallableResult> task) throws Exception {
-                                AlertDialog.Builder builder = new AlertDialog.Builder(ProfileUpdate.this);
+                                AlertDialog.Builder builder = new AlertDialog.Builder(InsertBmi.this);
 
                                 if (task.isSuccessful()){
                                     Map<String, Object> response = (Map<String, Object>) task.getResult().getData();
@@ -105,7 +74,5 @@ public class ProfileUpdate extends AppCompatActivity {
                         });
             }
         });
-
     }
-
 }
